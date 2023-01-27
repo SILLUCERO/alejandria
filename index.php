c0491a28e1486d8eeb3eb11b7f13dc99f8434a3a
SILLUCERO <silvinalaura@outlook.es>
2023-01-27 10:03:49 +0100

    Create cards

--- index.php
+++ index.php
@@ -5,8 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <title>Document</title>
+
 </head>
+
 <body>
     <nav>
     <div class="input-group mb-3">
@@ -17,8 +20,25 @@
     </div>
     </div>
     </nav>
+    <main>
+        <div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <i class="bi bi-trash3">delete</i>
+        <i class="bi bi-pencil-square">edit</i>
+    </div>
+    </div>
+    </main>
 
-        
+
+
+
+
+
+
             
 
 
