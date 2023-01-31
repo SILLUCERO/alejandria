ac7c1741713b0e3b157b11aab36d08dd65758742
IsabelGuMo <gm.isabel@gmail.com>
2023-01-31 18:59:32 +0100

    Styles at home page

--- index.php
+++ index.php
@@ -8,8 +8,9 @@ include_once('./conect.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles/styles.css">
+    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="styles/styles.css">
     <title>Document</title>
 </head>
 
@@ -23,23 +24,23 @@ include_once('./conect.php');
         </div>
     </nav>
     
-    <main>
+    <main class="product-list">
         <?php
             $result = $connect->query("SELECT * FROM alejandria");
         ?>
         
         <?php while($book = $result->fetch_assoc()): ?>
-        <div class="card" style="width: 18rem;">
-            <img src="assets/images/<?php echo $book['img']; ?>"  class="card-img-top" alt="<?php echo $book['titulo']; ?>">
-            <div class="card-body">
-                <h5 class="card-title"><?php echo $book['titulo']; ?></h5>
-                <p class="card-text"><?php echo $book['autor']; ?></p>
-                <p class="card-text"><?php echo $book['editorial']; ?></p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-                <i class="bi bi-trash3">delete</i>
-                <i class="bi bi-pencil-square">edit</i>
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/<?php echo $book['img']; ?>"  class="card-img-top" alt="<?php echo $book['titulo']; ?>">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $book['titulo']; ?></h5>
+                    <p class="card-text"><?php echo $book['autor']; ?></p>
+                    <p class="card-text"><?php echo $book['editorial']; ?></p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <i class="bi bi-trash3">delete</i>
+                    <i class="bi bi-pencil-square"></i>
+                </div>
             </div>
-        </div>
         <?php endwhile; ?>
     </main>
 </body>
