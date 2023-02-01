827ca31d668c253f6065d547301fff4d6d7fe837
IsabelGuMo <gm.isabel@gmail.com>
2023-02-01 21:35:35 +0100

    Create bookDetail

--- bookDetail.php
+++ bookDetail.php
@@ -11,7 +11,7 @@ include_once('./conect.php');
     
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <link rel="stylesheet" href="styles/styles.css">
-    <title>Document</title>
+    <title>BookDetail</title>
 </head>
 
 <body>
@@ -24,13 +24,13 @@ include_once('./conect.php');
         </div>
     </nav>
     
-    <main class="product-list">
+    <main>
         <?php
-            $result = $connect->query("SELECT * FROM alejandria");
+        $isbn = $_GET['isbn'];
+        $result = $connect->query("SELECT * FROM alejandria WHERE isbn = '$isbn'");
+        $book = $result->fetch_assoc();
         ?>
-        
-        <?php while($book = $result->fetch_assoc()): ?>
-
+    
         <div class="card" style="width: 18rem;">
         <?php if (isset($book['img']) && !empty($book['img']) && file_exists('assets/images/' . $book['img'])): ?>
             <img src="assets/images/<?php echo $book['img']; ?>" class="card-img-top" alt="<?php echo $book['titulo']; ?>">
@@ -41,7 +41,7 @@ include_once('./conect.php');
                 <h5 class="card-title"><?php echo $book['titulo']; ?></h5>
                 <p class="card-text"><?php echo $book['autor']; ?></p>
                 <p class="card-text"><?php echo $book['editorial']; ?></p>
-                <a href="bookDetail.php?isbn=<?php echo $book['isbn']; ?>" class="btn btn-primary">Go somewhere</a>
+                <a href="index.php" class="btn btn-primary">Go somewhere</a>
                 <!-- <a href="delete.php?isbn=<?php echo $book['isbn']; ?>">
                     <i class="bi bi-trash3">delete</i>
                 </a> -->
@@ -50,7 +50,6 @@ include_once('./conect.php');
                 </a>
             </div>
         </div>
-        <?php endwhile; ?>
     </main>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
